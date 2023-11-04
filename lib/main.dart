c061bb305df387fd6865493f804b0bc85ee92a73
Mohamed Ali <mo7amedaliebaid@gmail.com>
2023-11-04 23:43:56 +0200

    enable landscape

--- lib/main.dart
+++ lib/main.dart
@@ -36,10 +36,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
 
     return MultiProvider(
         providers: [
